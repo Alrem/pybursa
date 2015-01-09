18bfabad54ba6dbc4620058e361da0d7903ec03f
Aleksandr Kosse <aleksandr.kosse@gmail.com>
2015-01-09 17:50:44 +0200

    Add 400, 403, 404 and 500 error's page and locale_settings import

--- pybursa/settings.py
+++ pybursa/settings.py
@@ -144,3 +144,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+try:
+    from local_settings import *
+except ImportError:
+    print 'Warning! local_settings are not defined!'
